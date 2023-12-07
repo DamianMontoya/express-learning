33fa84f599eb653289f8d38cdf2da2ded9ae82a0
Damian Montoya <damian.montoya.dm@gmail.com>
2023-12-07 10:45:54 +0100

    agregadas diferentes rutas

--- server.js
+++ server.js
@@ -2,25 +2,62 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const port = 4004;
+const port = 4005;
+
+// files estaticos, dan acceso sin necesidad de ruta
+app.use(express.static(path.join(__dirname, 'public')));
+
+//middleware que me logea la url accesidad y el metodo
+app.use((req,res, next) =>
+{
+    console.log(`Route: ${req.url} with method ${req.method}`);
+    next();
+});
 
 app.get('/', (req, res) =>
 {
     console.log('Someone accessed to my home page');
+    res.sendFile(path.resolve(__dirname, './public/index.html'));
     res.status(200).send('This is my home page');
 });
 
 app.get('/login', (req,res) =>
 {
     console.log('Someone accessed to my log in page');
-    res.status(200).res.send('Log in page')
+    res.status(200).sendFile(path.resolve(__dirname,'./public/log-in.html'))
+});
+
+app.get('/contact', (req,res) => 
+{
+    console.log('Someone accessed my contact page');
+    res.status(200).sendFile(path.resolve(__dirname, './public/contact.html'));
+});
+
+// middleware que chekea si a la ruta /private se le pasa 'dami' como param query,
+// si es asi te da acceso, si no te redirije con un 302 a /
+app.use('/private', (req,res,next) =>
+{
+    if(req.query.param === 'dami')
+    {
+        next();
+    }
+    else
+    {
+        res.status(302).redirect('/');
+    }
+});
+
+app.get('/private', (req,res) =>
+{
+    res.status(200).sendFile(path.resolve(__dirname, './public/private.html'));
 });
 
 // si el usuario intenta acceder a cualquier recurso no definido devuelve un 404
 app.all('*', (req,res) =>
 {
+    console.log('Someone tried to acces to not defined resources');
     res.status(404).send('<h1>Resource is not available<h1>')
-})
+});
 
 app.listen(port, () =>
 {
